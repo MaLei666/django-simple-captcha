76e1ce840325de467705ef4fdd95120cfc12eed1
Nian <chengnian@zkfurong.com>
2018-06-07 17:00:34 +0800

    验证码

--- testproject/views.py
+++ testproject/views.py
@@ -1,14 +1,14 @@
 from django.http import HttpResponseRedirect
-from forms import CaptchaForm
+#from forms import CaptchaForm
+from . import forms
 from django.shortcuts import render
-
 
 def home(request):
     if request.POST:
-        form = CaptchaForm(request.POST)
+        form = forms.CaptchaForm(request.POST)
         if form.is_valid():
             return HttpResponseRedirect(request.path + '?ok')
     else:
-        form = CaptchaForm()
+        form = forms.CaptchaForm()
 
     return render(request, 'home.html', {'form': form})
